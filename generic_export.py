cde68e5f548ff84ea947d119e3e4411ff235e230
Blue Prawn <monnier.florent@gmail.com>
2013-08-28 13:03:46 +0200

    switch from GPL to Zlib license

--- generic_export.py
+++ generic_export.py
@@ -1,18 +1,13 @@
 #!BPY
 # Copyright (C) 2010 Florent Monnier
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This software is provided "AS-IS", without any express or implied
+# warranty.  In no event will the authors be held liable for any damages
+# arising from the use of this software.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Permission is granted to anyone to use this software for any purpose,
+# including commercial applications, and to alter it and redistribute it
+# freely.
 
 """
 Name: 'Generic Exporter'
